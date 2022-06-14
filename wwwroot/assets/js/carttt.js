b1477ee2e0d4eea323a8ecfaecb6e93853604288
Javid-Git <cavid.alizade00@mail.ru>
2022-06-14 09:16:00 +0400

    bacpac

--- wwwroot/assets/js/carttt.js
+++
@@ -1,8 +0,0 @@
-$('.product-close').click(function (e) {
-    e.preventDefault();
-    let url = $(this).attr('href');
-    console.log('sadasasa');
-
-
-    fetch(url).then(response => response.text()).then(data => { $('.mini-cart').html(data) })
-})
--- wwwroot/assets/js/search.js
+++ wwwroot/assets/js/search.js
@@ -1,58 +1,61 @@
 
-$('.modaldetail').click(function (e) {
-    e.preventDefault();
-    let url = $(this).attr('href');
+$(document).ready(function () {
+    $('.modaldetail').click(function (e) {
+        e.preventDefault();
+        let url = $(this).attr('href');
 
-    console.log(url);
+        console.log(url);
 
-    fetch(url).then(response => {
-        return response.text();
-    }).then(data => {
-        $('.modal-content').html(data);
+        fetch(url).then(response => {
+            return response.text();
+        }).then(data => {
+            $('.modal-content').html(data);
+        })
     })
-})
-$('.searchinput').keyup(function () {
-    let value = $(this).val();
-    console.log(value);
+    $('.searchinput').keyup(function () {
+        let value = $(this).val();
+        console.log(value);
 
-    let url = $(this).data("url");
-    url = url + '?search=' + value.trim();
-    console.log(url);
+        let url = $(this).data("url");
+        url = url + '?search=' + value.trim();
+        console.log(url);
 
-    if (value) {
+        if (value) {
+            fetch(url)
+                .then(response => response.text())
+
+                .then(data => {
+                    $('.search-body .list-group').html(data)
+
+                })
+        }
+        else {
+            $('.search-body .list-group').html('')
+        }
+
+    })
+
+    $('.addtobasket').click(function (e) {
+        e.preventDefault();
+        let url = $(this).attr('href');
+
         fetch(url)
             .then(response => response.text())
-
             .then(data => {
-                $('.search-body .list-group').html(data)
-
+                $('.mini-cart').html(data)
             })
-    }
-    else{
-        $('.search-body .list-group').html('')
-    }
-    
+    })
+
+    $('.deletefrombasket').click(function (e) {
+        e.preventDefault();
+        let url = $(this).attr('href');
+        console.log('aaaaaaaaa');
+
+        fetch(url)
+            .then(response => response.text())
+            .then(data => {
+                $('.mini-cart').html(data)
+            })
+    })
 })
 
-$('.addtobasket').click(function (e) {
-    e.preventDefault();
-    let url = $(this).attr('href');
-
-    fetch(url)
-        .then(response => response.text())
-        .then(data => {
-            $('.mini-cart').html(data)
-        })
-})
-
-$('.deletefrombasket').click(function (e) {
-    e.preventDefault();
-    let url = $(this).attr('href');
-    console.log('aaaaaaaaa');
-
-    fetch(url)
-        .then(response => response.text())
-        .then(data => {
-            $('.mini-cart').html(data)
-        })
-})
